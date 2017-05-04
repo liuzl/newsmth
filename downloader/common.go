95d9f1306be405ea73f66bb0aee01fb06431bdf6
zliu <zhanliangliu@qq.com>
2017-05-04 11:44:38 +0800

    common structures definiation

---
+++ downloader/common.go
@@ -0,0 +1,44 @@
+package downloader
+
+import "fmt"
+
+type RequestInfo struct {
+	Url       string
+	Method    string
+	PostData  string
+	UseProxy  bool
+	NeedLogin bool
+	Proxy     string
+	Cookies   map[string]string
+	Timeout   int
+	MaxLen    int64
+	Platform  string
+}
+
+func (req *RequestInfo) String() string {
+	return fmt.Sprintf("url: %s, post data: %s, proxy: %s", req.Url, req.PostData, req.Proxy)
+}
+
+type ResponseInfo struct {
+	Url        string
+	Text       string
+	Content    []byte
+	Encoding   string
+	StatusCode int
+	Proxy      string
+	Cookies    map[string]string
+	Error      *DownloaderError
+}
+
+func (resp *ResponseInfo) String() string {
+	return fmt.Sprintf("url: %s, status: %d, proxy: %s, error: %s", resp.Url, resp.StatusCode, resp.Proxy, resp.Error)
+}
+
+type DownloaderError struct {
+	Code    int
+	Message string
+}
+
+func (e DownloaderError) Error() string {
+	return fmt.Sprintf("code: %d, message: %s", e.Code, e.Message)
+}
