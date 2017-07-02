7d69f7cf7cdb3f9af33508ee80de49adb770cf41
liuzl <zhanliangliu@qq.com>
2017-07-02 21:44:22 +0800

    util.go

---
+++ tests/server_test/util.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/golang/glog"
+	"net/http"
+)
+
+func showError(w http.ResponseWriter, r *http.Request, msg string, code int) {
+	glog.Error("Reporting error ", code, "/", msg)
+	http.Error(w, msg, code)
+}
+
+func mustEncode(w http.ResponseWriter, i interface{}) {
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Content-type", "application/json")
+	e := json.NewEncoder(w)
+	if err := e.Encode(i); err != nil {
+		panic(err)
+	}
+}
+
+func ok(w http.ResponseWriter) {
+	rv := struct {
+		Status string `json:"status"`
+	}{
+		Status: "ok",
+	}
+	mustEncode(w, rv)
+}
